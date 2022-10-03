043308a1686a18a7e5bb92ecb7241dbd95f51326
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-03 17:23:28 +0600

    UnControl Form done

--- src/components/UnControlForm.jsx
+++ src/components/UnControlForm.jsx
@@ -5,41 +5,50 @@ export default class UnControlForm extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
+
+        const formObj = {}
+
+        formObj.name = event.target.name.value
+        formObj.email = event.target.email.value
+        formObj.password = event.target.password.value
+        console.log(formObj)
+
+        event.target.reset()
     }
 
   render() {
     return (
         <div>
-            <form className='unControlForm'>
-            <input
-                type="text"
-                name='name'
-                placeholder='Name'
-            />
-            <br />
-            <input
-                type="email"
-                name='email'
-                placeholder='email@example.com'
-            />
-            
-            <br />
+            <form
+                onSubmit={this.handleSubmit}
+                className='unControlForm'>
+                <input
+                    type="text"
+                    name='name'
+                    placeholder='Name'
+                />
+                <br />
+                <input
+                    type="email"
+                    name='email'
+                    placeholder='email@example.com'
+                />
                 
-            <input
-                type="password"
-                name='password'
-                placeholder='*******'
-            />
-            <br />
+                <br />
+                    
+                <input
+                    type="password"
+                    name='password'
+                    placeholder='*******'
+                />
+                <br />
 
-            <button
-                type='submit'
-
-             >Submit </button>
+                <button>Submit</button>
+                
             </form>
-            <button>
-                Sign Up
-            </button>
+
+
+            
       </div>
     )
   }
