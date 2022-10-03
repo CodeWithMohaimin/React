5d1d8b20f649923ecc7caa65b43b4cd0947efa07
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-03 08:38:40 +0600

    control form

--- src/components/ControlForm.jsx
+++ src/components/ControlForm.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export default class ControlForm extends Component {
+
+  state = {
+    name: '',
+    email: '',
+  }
   render() {
     return (
       <div>
-        ControlForm
+        
+
       </div>
     )
   }
