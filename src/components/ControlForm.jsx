03094345286783efbbd6739117d522163e4146e6
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-03 08:17:10 +0600

    fdef

---
+++ src/components/ControlForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ControlForm extends Component {
+  render() {
+    return (
+      <div>
+        ControlForm
+      </div>
+    )
+  }
+}
