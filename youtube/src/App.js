8270209e7c9f73dbbd8da577456b663f96ce3f48
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-14 07:26:42 +0600

    learning from youtube

---
+++ youtube/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-black text-white">
+        hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
