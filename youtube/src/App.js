3fc83ce66729104987d4d5450437f9463964d5d5
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-14 12:44:45 +0600

    post showing using for loop

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
+import React from 'react';
+import Post from './components/Post';
+import Data from './components/Data.json';
+
+
+let items = []
+for (let i = 0; i < Data.length; i++) {
+  items.push(
+    <Post
+      userId={Data[i].userId}
+      postId={Data[i].postId}
+      title={Data[i].title}
+      body = {Data[i].body}
+    />
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-black text-white">
-        hello world!
-      </h1>
+      {/* <Post
+        userId={Data[0].userId}
+        postId={Data[0].postId}
+        title={Data[0].title}
+        body = {Data[0].body}
+      /> */}
+      {items}
     </div>
   );
 }
---
+++ youtube/src/components/Post.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const Post = (props) => {
+  // const items = [];
+  // items = Data.map(data =>data.title)
+  // console.log(items)
+
+  return (
+    <div className='flex flex-col justify-center w-full max-w-md mx-auto my-10 bg-teal-500 p-8 space-y-4 text-lg text-white'>
+      <p>User ID : {props.userId}</p>
+      <p>Post Number :{props.postId}</p>
+
+      <h1 className='bg-teal-600 p-2 text-center'>Title</h1>
+      <p className='text-center capitalize'>
+        {props.title}
+      </p>
+
+      <h1 className='bg-teal-400 p-2 text-center'>Body</h1>
+      <p className='text-center capitalize'>
+        {props.body}
+      </p>
+      
+    </div>
+  )
+}
+
+export default Post
