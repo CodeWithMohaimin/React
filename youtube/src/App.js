74b711814ceebd860dde81fe8b0cf5c009f178f4
programmermohaimin <dev.mohaimin@gmail.com>
2022-10-16 10:08:27 +0600

    mastering map

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import Post from './components/Post';
-import Data from './components/Data.json';
 
 
-let items = []
-
-items= Data.map((post) => <Post
-        userId={post.userId}
-        postId={post.postId}
-        title={post.title}
-        body = {post.body}
-      />)
-
-// for (let i = 0; i < Data.length; i++) {
-//   items.push(
-//     <Post
-//       userId={Data[i].userId}
-//       postId={Data[i].postId}
-//       title={Data[i].title}
-//       body = {Data[i].body}
-//     />
-//   )
-// }
 
 function App() {
   return (
     <div className="App">
-      {items}
+      <Post />
     </div>
   );
 }
--- youtube/src/components/Post.jsx
+++ youtube/src/components/Post.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Data from './Data.json';
 
 
-const Post = (props) => {
+
+
+const Post = () => {
   return (
-    <div className='flex flex-col justify-center w-full max-w-md mx-auto my-10 bg-teal-500 p-8 space-y-4 text-lg text-white'>
-      <p>User ID : {props.userId}</p>
-      <p>Post Number :{props.postId}</p>
-
-      <h1 className='bg-teal-600 p-2 text-center'>Title</h1>
-      <p className='text-center capitalize'>
-        {props.title}
-      </p>
-
-      <h1 className='bg-teal-400 p-2 text-center'>Body</h1>
-      <p className='text-center capitalize'>
-        {props.body}
-      </p>
+    <div>
+      {Data.map((item, index) =>
+        <div key={index} className='flex flex-col justify-center w-full max-w-md mx-auto my-10 bg-teal-500 p-8 space-y-4 text-lg text-white'>
+          <p>User ID : {item.userId}</p>
+          <p>Post Number :{item.postId}</p>
+  
+          <h1 className='bg-teal-600 p-2 text-center'>Title</h1>
+          <p className='text-center capitalize'>
+          {item.title}
+          </p>
+  
+          <h1 className='bg-teal-400 p-2 text-center'>Body</h1>
+          <p className='text-center capitalize'>
+          {item.body}
+          </p>
+        </div>
+      )} 
     </div>
   )
 }
