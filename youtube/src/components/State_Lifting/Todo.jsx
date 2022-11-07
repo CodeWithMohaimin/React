2631d7d322dc5b4a0849f0cb7b80d41dc50eabe4
programmermohaimin <dev.mohaimin@gmail.com>
2022-11-07 19:53:58 +0600

    starting a stycky navbar

--- youtube/src/components/State_Lifting/Todo.jsx
+++ youtube/src/components/State_Lifting/Todo.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Todo = (props) => {
   return (
-    <div>
+    <div className='flex justify-between'>
       <h1 className='m-2 text-left bg-gray-200 p-2 rounded'>{props.index + 1} . {props.todo}</h1>
+
+      <button className='bg-red-300 my-2 px-2 '>Delete</button>
     </div>
   )
 }
